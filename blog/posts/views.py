c085373f604047ee4afb6b378243af9432e47a0f
Alexandr Zemlyanskiy <sasha.zemlyanskiy.00@mail.ru>
2022-02-23 21:03:54 +0300

    classwork

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,13 +1,16 @@
 import logging
+
 from django.http import HttpResponse
+
+from posts.models import Post
 
 logger = logging.getLogger(__name__)
 
 
 def posts_index(request):
-    value = request.GET.get("key")
-    logger.info(value)
-    return HttpResponse("Posts index view")
+    author_name = request.GET.get("author", "zemlik")
+    posts = Post.objects.filter(author_username=author_name)
+    return HttpResponse(posts)
 
 
 
